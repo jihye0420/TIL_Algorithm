cd1c22e2d753aba9d2eaa393791d3d05b55d3773
jihye0420 <20181054@sungshin.ac.kr>
2023-01-24 20:50:01 +0900

    ADD: 문제 테스트 커밋

---
+++ 1000.py
@@ -0,0 +1,21 @@
+# a,b = map(int, input().split())
+# print(a+b)
+
+# a,b = map(int, input().split())
+# print(a-b)
+
+# a,b = map(int, input().split())
+# print(a*b)
+
+# a,b = map(int, input().split())
+# print(a/b)
+
+# a,b = map(int, input().split())
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(int(a/b))
+# print(a%b)
+
+name = input().join('??!')
+print(name)
