6624c2a75be6686649efdd160ec77832baa407c7
jihye0420 <20181054@sungshin.ac.kr>
2023-01-30 03:07:34 +0900

    :fire: [숫자 문자열과 영단어] 구현 중

---
+++ Programmers/Lv.1/숫자 문자열과 영단어.py
@@ -0,0 +1,4 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/81301
+def solution(s):
+    answer = 0
+    return answer
---
+++ Programmers/Lv.1/예산.py
@@ -0,0 +1,20 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12982
+def solution(d, budget):
+    answer = 0
+    d.sort()
+    for i in d:
+        if budget-i < 0:
+            break
+        else:
+            budget -= i
+            answer += 1
+    return answer
+
+
+if __name__ == '__main__':
+    print(solution([1, 3, 2, 5, 4], 9))
+    print(solution([2,2,3,3], 10))
+
+# d = [1, 3, 2, 5, 4]
+# for i in d:
+#     print(i)
