da7dd292a91ffe0a4d8943043c5382134d77ac5d
jihye0420 <20181054@sungshin.ac.kr>
2023-02-07 19:02:45 +0900

    :white_check_mark: [영어 끝말잇기] 리스트 이용하기 구현완료

--- Programmers/Lv.2/영어 끝말잇기.py
+++ Programmers/Lv.2/영어 끝말잇기.py
@@ -2,23 +2,19 @@
 def solution(n, words):
     answer = []
     temp = []
-    tmp = 0
-
-    for i in words:
-        tmp += 1
-        if temp:
-            if i in temp:
-                break
-            if temp[-1][-1] != i[0]:
-                break
-        temp.append(i)
-    # print(tmp)
-    answer.append(tmp // n)
-    return answer
+    temp.append(words[0])
+    number, order = 0, 0
+    for i in range(1, len(words)):
+        if words[i] in temp or temp[-1][-1] != words[i][0]:
+            number = (i % n) + 1
+            order = (i // n) + 1
+            break
+        temp.append(words[i])
+    return [number, order]
 
 
 if __name__ == '__main__':
     print(solution(3, ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]))
-    # print(solution(5, ["hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish",
-    #                    "hang", "gather", "refer", "reference", "estimate", "executive"]))
+    print(solution(5, ["hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish",
+                       "hang", "gather", "refer", "reference", "estimate", "executive"]))
     print(solution(2, ["hello", "one", "even", "never", "now", "world", "draw"]))
