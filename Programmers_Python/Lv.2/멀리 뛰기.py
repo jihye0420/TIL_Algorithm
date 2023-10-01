ad8f60ab91ed544525d0f7ea168642706f8c90f0
jihye0420 <20181054@sungshin.ac.kr>
2023-10-02 00:09:40 +0900

    [프로그래머스] Lv2.멀리 뛰기 테스트 케이스 완료, 코드 개선 필요

--- Programmers_Python/Lv.2/멀리 뛰기.py
+++ Programmers_Python/Lv.2/멀리 뛰기.py
@@ -5,23 +5,24 @@ from itertools import permutations, product
 def solution(n):
     answer = 0
     # 1 또는 2로 구성 가능!
-
     sets = [1, 2]
-    # result_3 = list(product(data, repeat=2))
-    data = [0] * 10000
+
+    data = []
 
     for i in range(1, n + 1):
-        data[i] = list(product(sets, repeat=i))
-
+        data.append(list(product(sets, repeat=i)))
     # print(data)
-    for i in data:
-        print(i)
-        # if sum(i) == n:
-        #     answer += 1
+    # print(len(data))
 
+    for j in range(0, n):
+        # print(data[j])
+        for k in data[j]:
+            # print(k)
+            if sum(k) == n:
+                answer += 1
     return answer
 
 
 if __name__ == '__main__':
     print(solution(4))
-    # print(solution(3))
+    print(solution(3))
