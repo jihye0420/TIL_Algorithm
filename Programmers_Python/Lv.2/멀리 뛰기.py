a7e9d2001fc5358fd01907274f4b13bac42a6f0d
jihye0420 <20181054@sungshin.ac.kr>
2023-10-03 23:01:47 +0900

    [프로그래머스] LV2.멀리뛰기 DP 테이블 이용하기

--- Programmers_Python/Lv.2/멀리 뛰기.py
+++ Programmers_Python/Lv.2/멀리 뛰기.py
@@ -1,26 +1,46 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12914
 from itertools import permutations, product
 
+"""
+나의 풀이
+"""
+# def solution(n):
+#     answer = 0
+#     # 1 또는 2로 구성 가능!
+#     sets = [1, 2]
+#
+#     data = []
+#
+#     for i in range(1, n + 1):
+#         data.append(list(product(sets, repeat=i)))
+#     print("data: ", data)
+#     print(len(data))
+#
+#     for j in range(0, n):
+#         # print(data[j])
+#         for k in data[j]:
+#             # print(k)
+#             if sum(k) == n:
+#                 answer += 1
+#     return answer
+
+
+"""
+다른 풀이
+"""
+
 
 def solution(n):
-    answer = 0
-    # 1 또는 2로 구성 가능!
-    sets = [1, 2]
+    # n까지 뛰는 방법을 저장하기 위한 배열
+    dp = [0] * (n + 1)
 
-    data = []
+    dp[0] = 1
+    dp[1] = 2
 
-    for i in range(1, n + 1):
-        data.append(list(product(sets, repeat=i)))
-    # print(data)
-    # print(len(data))
+    for i in range(2, n):
+        dp[i] = dp[i - 2] + dp[i - 1]
 
-    for j in range(0, n):
-        # print(data[j])
-        for k in data[j]:
-            # print(k)
-            if sum(k) == n:
-                answer += 1
-    return answer
+    return dp[n - 1] % 1234567
 
 
 if __name__ == '__main__':
