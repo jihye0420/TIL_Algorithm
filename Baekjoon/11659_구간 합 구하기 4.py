bfd52d5713a5498279d8fadc73675aded3eaf845
jihye0420 <20181054@sungshin.ac.kr>
2023-10-09 23:56:47 +0900

    :poop: 열심히 해야한다.

--- Baekjoon/11659_구간 합 구하기 4.py
+++ Baekjoon/11659_구간 합 구하기 4.py
@@ -2,17 +2,20 @@
 n, m = map(int, input().split())
 numbers = list(map(int, input().split()))
 
-s = [0] * n
-s = numbers[]
+s = [0]
 # s[0] = numbers[0]
-for i in range(1, len(numbers)):
-    s[i] = s[i - 1] + numbers[i]
+# for i in range(1, len(numbers)):
+#     s[i] = s[i - 1] + numbers[i]
+temp = 0
+for i in numbers:
+    temp = temp + i
+    s.append(temp)
 
 answer = []
 for _ in range(m):
     i, j = map(int, input().split())
     answer.append(s[j] - s[i - 1])
-print(answer)
+# print(answer)
 
 for k in answer:
     print(k)
