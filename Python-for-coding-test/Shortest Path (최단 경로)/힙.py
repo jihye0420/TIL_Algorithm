9e6280c842636c21b7324d5b5a47085ac8610688
jihye0420 <20181054@sungshin.ac.kr>
2023-10-15 23:30:53 +0900

    :poop: [자료구조] 힙 라이브러리 사용, 최소힙 구현
    힘들다. 집중해야해. 얼마나 열심히 해야하는거야!!!!!!!!!!!!

---
+++ Python-for-coding-test/Shortest Path (최단 경로)/힙.py
@@ -0,0 +1,19 @@
+# * 최소 힙: 값이 작은 데이터가 먼저 삭제
+import heapq
+
+
+# 오름차순 힙 정렬(Heap Sort)
+def heapsort(iterable):
+    h = []
+    result = []
+    # 모든 원소를 차례대로 힙에 삽입
+    for value in iterable:
+        heapq.heappush(h, value)
+    # 힙에 삽입된 모든 원소를 차례대로 꺼내어 담기
+    for i in range(len(h)):
+        result.append(heapq.heappop(h))
+    return result
+
+
+result = heapsort([1, 3, 5, 7, 9, 2, 4, 6, 8, 0])
+print(result)
