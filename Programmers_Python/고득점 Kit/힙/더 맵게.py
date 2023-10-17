fbb29599d76e283b7604dc82dd5af131c1b4ed00
jihye0420 <20181054@sungshin.ac.kr>
2023-10-17 21:50:15 +0900

    [고득점 kit-힙] LV.2 더 맵게, 풀이 문제 파악 중

--- Programmers_Python/고득점 Kit/힙/더 맵게.py
+++ Programmers_Python/고득점 Kit/힙/더 맵게.py
@@ -1,27 +1,61 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42626
+"""
+# * 나의 풀이 => 테스트 케이스 실패 (정확성, 효율성 실패)
+# 정확성: 48.4
+# 효율성: 0.0
+# 합계: 48.4 / 100.0
+"""
 import heapq
 
 
 def solution(scoville, k):
     answer = 0
-    h = []
-    result = []
-    for i in scoville:
-        heapq.heappush(h, i)
-    # for j in range(len(h)):
-    #     result.append(heapq.heappop(h))
+
+    # h = []
+    # result = []
+    # for i in scoville:
+    #     heapq.heappush(h, i)
+
+    # 힙을 만드는 과정이 아래 코드로 대체됨
+    heapq.heapify(scoville)
+
     hot = 0
-    while h:
-        a = heapq.heappop(h)
-        b = heapq.heappop(h)
+    while scoville[0] < k:
+        a = heapq.heappop(scoville)
+        b = heapq.heappop(scoville)
         hot = a + b * 2
         answer += 1
         if hot >= k:
             break
         else:
-            heapq.heappush(h, hot)
-
+            heapq.heappush(scoville, hot)
+        # ! 아래 h의 개수, k보다 작은지의 여부를 확인 =>
+        if len(scoville) == 1 and scoville[0] < k:
+            return -1
     return answer
+
+
+"""
+# * 다른 사람 풀이
+"""
+
+# ! 구현
+# import heapq
+#
+#
+# def solution(scoville, k):
+#     answer = 0
+#     # 기존 리스트 -> 최소 힙으로!
+#     heapq.heapify(scoville)
+#
+#     #
+#     while scoville[0] < k:
+#         mix = heapq.heappop(scoville) + (heapq.heappop(scoville) * 2)
+#         heapq.heappush(scoville, mix)
+#         answer += 1
+#         if len(scoville) == 1 and scoville[0] < k:
+#             return -1
+#     return answer
 
 
 if __name__ == '__main__':
--- Python-for-coding-test/Shortest Path (최단 경로)/힙.py
+++ Python-for-coding-test/Shortest Path (최단 경로)/힙.py
@@ -36,3 +36,24 @@ def heapsort(iterable):
 
 result = heapsort([1, 3, 5, 7, 9, 2, 4, 6, 8, 0])
 print(result)
+
+"""
+# 기존 리스트를 힙으로 변환
+"""
+import heapq
+
+# 최소 힙 생성, push
+heap_list = []
+heapq.heappush(heap_list, 4)
+heapq.heappush(heap_list, 1)
+heapq.heappush(heap_list, 7)
+
+# pop
+heapq.heappop(heap_list)
+
+# pop하지 않고 최솟값 얻기
+print(heap_list[0])
+
+# 기존 리스트를 힙으로 변환
+a_list = [4, 1, 7, 3, 8, 5]
+heapq.heapify(a_list)
