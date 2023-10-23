0fde27a6d3398d23605997c211f76e33f0d42c5a
jihye0420 <20181054@sungshin.ac.kr>
2023-10-23 22:32:13 +0900

    [고득점 kit-DFS, BFS] 네트워크 테케 통과, 정확성 풀이 필요

--- Programmers_Python/고득점 Kit/깊이, 너비 우선 탐색 (DFS,BFS)/네트워크.py
+++ Programmers_Python/고득점 Kit/깊이, 너비 우선 탐색 (DFS,BFS)/네트워크.py
@@ -1,8 +1,38 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/43162
+"""
+# * 나만의 풀이 => DFS로 해결
+테스트 케이스는 다 맞음
+정확성: 15.4
+합계: 15.4 / 100.0
+"""
+
+
+def dfs(x, y, computers, n):
+    if x < 0 or y < 0 or x >= n or y >= n:
+        return False
+    if computers[x][y] == 1:
+        # 방문처리
+        computers[x][y] = 0
+        # 상하좌우 모두 재귀적으로 호출!
+        dfs(x - 1, y, computers, n)
+        dfs(x + 1, y, computers, n)
+        dfs(x, y - 1, computers, n)
+        dfs(x, y + 1, computers, n)
+        return True
+    return False
+
+
 def solution(n, computers):
     answer = 0
+    # visited = [0] * n
+    # dfs(computers, 0, visited)
+    for i in range(n):
+        for j in range(n):
+            if dfs(i, j, computers, n) == True:
+                answer += 1
     return answer
 
 
 if __name__ == '__main__':
-    print(solution([1, 1, 1, 1, 1], 3))
+    print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
+    print(solution(3, [[1, 1, 0], [1, 1, 1], [0, 1, 1]]))
