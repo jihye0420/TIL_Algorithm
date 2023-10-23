6bd71b891dd5bbcf7cbc276dd297bb496627da69
jihye0420 <20181054@sungshin.ac.kr>
2023-10-23 22:46:43 +0900

    [고득점 kit-DFS, BFS] 네트워크 DFS 풀이

--- Programmers_Python/고득점 Kit/깊이, 너비 우선 탐색 (DFS,BFS)/네트워크.py
+++ Programmers_Python/고득점 Kit/깊이, 너비 우선 탐색 (DFS,BFS)/네트워크.py
@@ -6,30 +6,50 @@
 합계: 15.4 / 100.0
 """
 
+# def dfs(x, y, computers, n):
+#     if x < 0 or y < 0 or x >= n or y >= n:
+#         return False
+#     if computers[x][y] == 1:
+#         # 방문처리
+#         computers[x][y] = 0
+#         # 상하좌우 모두 재귀적으로 호출!
+#         dfs(x - 1, y, computers, n)
+#         dfs(x + 1, y, computers, n)
+#         dfs(x, y - 1, computers, n)
+#         dfs(x, y + 1, computers, n)
+#         return True
+#     return False
+#
+#
+# def solution(n, computers):
+#     answer = 0
+#     # visited = [0] * n
+#     # dfs(computers, 0, visited)
+#     for i in range(n):
+#         for j in range(n):
+#             if dfs(i, j, computers, n) == True:
+#                 answer += 1
+#     return answer
 
-def dfs(x, y, computers, n):
-    if x < 0 or y < 0 or x >= n or y >= n:
-        return False
-    if computers[x][y] == 1:
-        # 방문처리
-        computers[x][y] = 0
-        # 상하좌우 모두 재귀적으로 호출!
-        dfs(x - 1, y, computers, n)
-        dfs(x + 1, y, computers, n)
-        dfs(x, y - 1, computers, n)
-        dfs(x, y + 1, computers, n)
-        return True
-    return False
+"""
+# ! 다른 풀이
+"""
 
 
 def solution(n, computers):
+    def DFS(i):
+        visited[i] = 1
+        for a in range(n):
+            if computers[i][a] and not visited[a]:
+                DFS(a)
+
     answer = 0
-    # visited = [0] * n
-    # dfs(computers, 0, visited)
+    visited = [0 for i in range(len(computers))]
     for i in range(n):
-        for j in range(n):
-            if dfs(i, j, computers, n) == True:
-                answer += 1
+        if not visited[i]:
+            DFS(i)
+            answer += 1
+
     return answer
 
 
