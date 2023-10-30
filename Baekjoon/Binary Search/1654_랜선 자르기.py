27f55a2be6cc44e8aa33fb3c7ff176d0ac32ef99
jihye0420 <20181054@sungshin.ac.kr>
2023-10-31 02:55:58 +0900

    :white_check_mark: [백준 - 이분탐색] 1654 랜선 자르기 solved
    left, right 설정 코드 & result값을 left or right로 출력하는 부분 보기

--- Baekjoon/Binary Search/1654_랜선 자르기.py
+++ Baekjoon/Binary Search/1654_랜선 자르기.py
@@ -4,6 +4,8 @@
 * 아이디어
 - 이분 탐색 : 런타임 에러 발생...
 * 알게된 점
+- left, right 설정 # 최소 1로 시작하는 부분.....문제가 뭐냐....??? # todo: 다시보기
+- todo: 왜 right값을 print()?
 """
 import sys
 
@@ -15,18 +17,17 @@ data = [int(input()) for i in range(k)]
 #     data.append(int(input()))
 data.sort()
 
-result = 0  # mid값을 저장
-left, right = 0, max(data)
+# result = 0  # mid값을 저장
+left, right = 1, max(data)  # 최소 1로 시작하는 부분.....문제가 뭐냐....??? # todo: 다시보기
 
 while left <= right:
     total = 0  # 총 개수
     mid = (left + right) // 2
     for i in data:
-        if i // mid > 0:
-            total += i // mid
+        total += i // mid
     if total >= n:  # 필요한 랜선의 개수가 많다면 - 자르는 단위의 길이가 짧다는 것  => 오른쪽 탐색
-        result = mid
+        # result = mid
         left = mid + 1
     else:  # 필요한 랜선의 개수가 적다면 - 자르는 단위의 길이가 길다는 것 (최대 랜선의 길이 저장) => 왼쪽 탐색
         right = mid - 1
-print(result)
+print(right)
