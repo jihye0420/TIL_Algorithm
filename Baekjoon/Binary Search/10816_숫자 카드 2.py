964d280b27e74fbc892cb33456821a116e38e862
jihye0420 <20181054@sungshin.ac.kr>
2023-10-30 14:33:19 +0900

    [백준 - 이분탐색] 10816 숫자카드2 이분 탐색 적용 전, 딕셔너리로 solved

--- Baekjoon/Binary Search/10816_숫자 카드 2.py
+++ Baekjoon/Binary Search/10816_숫자 카드 2.py
@@ -4,25 +4,54 @@
 # 8
 # 10 9 -5 2 3 4 5 -10
 """
-나만의 풀이 => 시간초과ㄴ
+나만의 풀이 => 시간초과
+1. 리스트 => 시간초과
+2. 딕셔너리로 풀어보기
 """
 n = int(input())
-data = list(map(int, input().split()))
-# data = {}
-# data
-
+# data = list(map(int, input().split()))
+temp = list(map(int, input().split()))
+data = {}
 m = int(input())
 check = list(map(int, input().split()))
 
+for i in temp:
+    if i not in data.keys():
+        data[i] = 1
+    else:
+        data[i] += 1
+
 for i in check:
-    print(data.count(i), end=' ')
+    print(data[i] if i in data.keys() else 0, end=' ')
 """
 이분 탐색
 """
-n = int(input())
-data = list(map(int, input().split()))
-
-m = int(input())
-check = list(map(int, input().split()))
-
-# while
+# n = int(input())
+# data = list(map(int, input().split()))
+# data.sort()
+#
+# m = int(input())
+# check = list(map(int, input().split()))
+#
+# for i in check:
+#     count = 0
+#     check_num = 0
+#     left = 0
+#     right = n - 1
+#
+#     while left <= right:
+#         mid = (left + right) // 2
+#         if i == data[mid]:
+#             count += 1
+#             for j in data[:mid]:
+#                 if i == j:
+#                     count += 1
+#             for j in data[mid + 1:]:
+#                 if i == j:
+#                     count += 1
+#             break
+#         elif i < data[mid]:
+#             right = mid - 1
+#         elif i > data[mid]:
+#             left = mid + 1
+#     print(count, end=' ')
